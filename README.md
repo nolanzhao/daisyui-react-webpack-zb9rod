109aae55a754c07c058810dd5826f715a96f1c35
nolanzhao <z_nolan@126.com>
2022-11-17 11:50:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-webpack-zb9rod
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-webpack-zb9rod)
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+
+const App = (props) => {
+  return (
+    <div>
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+};
+export default App;
